fcf886d8a3f0e2cd9ab3d70042a18abd8e8985d6
adminerest <liuchunpeng1@hotmail.com>
2020-01-03 13:40:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ if __name__ == "__main__":
         具体参照example.csv中格式
         下方dealType是选课的类型
         务必确保所选课程和课程类型一致！！！
-        现在目测3是校任选课（大概也就是任选课），2是专业选修
+        现在目测3是校任选课（大概也就是任选课），2是专业选修（包括体育课）
+        不过好像2的话校任选和专业选修都可以选。。。
         重修请自己选课。。。
     """
-    dealType = "3"
+    dealType = "2"
     lessons = Lessons(dealType)
     lessons.auto_spider()  # 开始自动选课
     print("全部课程已成功选上！请去urp查收！")
